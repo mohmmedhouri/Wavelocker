8d999f65a18a65cb5e07f56fccdac26729942946
mohmmedhouri <31990025+mohmmedhouri@users.noreply.github.com>
2018-02-08 10:07:06 +0100

    Adding tracking methode using shift to maxima
    It seems it works but it has some issue it needs to increase the AMCC modulation

--- Wave_locker_trail.c
+++ Wave_locker_trail.c
@@ -47,7 +47,10 @@ int main(int argc, char **argv){
 		float avrEta=0;
 		float avr=0;
 
-				
+	float Demoarray [5];
+
+	for (c=0;c<255;c++)
+	CH1.obj[c].power=0;			
 
 		
         float *buff  = (float *)malloc(Num_SAMPLES * sizeof(float)); // Allocate space for for 1st ADC 1 for reference 
@@ -136,7 +139,7 @@ int main(int argc, char **argv){
 
 	case '1' : // tunning
 		
-		for (i=128;i<255;i++)
+		for (i=128;i<168;i++)
 		{	
 			avrRef=0;
 			avrEta=0;
@@ -192,15 +195,15 @@ int main(int argc, char **argv){
 
 			CH1.obj[i].power =  avr/50;
 			
-			seq_gen_mc(PosShi6,810,1);
+			seq_gen_mc(PosShi20,810,1);
 			
 			printf("%f  %f  %f \n",CH1.obj[i].ref,CH1.obj[i].etalon,CH1.obj[i].power);
 
 		} // end of for or positive tune
 
-		for (i=0;i<128;i++)
+		for (i=0;i<40;i++)
 		{
-		 	seq_gen_mc(NegShi6,810,1);
+		 	seq_gen_mc(Negshi20,810,1);
 			usleep(68800);
 		} 
 
@@ -214,7 +217,7 @@ int main(int argc, char **argv){
 
 
 		
-		 for (i=127;i>0;i--)
+		 for (i=127;i>87;i--)
          {
 			 avrRef=0;
 			 avrEta=0;
@@ -267,15 +270,15 @@ int main(int argc, char **argv){
 
 			CH1.obj[i].power =  avr/50;
 			
-			seq_gen_mc(NegShi6,810,1);
+			seq_gen_mc(Negshi20,810,1);
 			
                        
 
             } // end of for or negative tune
 			
-			for (i=0;i<128;i++)
+			for (i=0;i<40;i++)
 			{
-				seq_gen_mc(PosShi6,810,1);
+				seq_gen_mc(PosShi20,810,1);
 				usleep(68800);
 
 			}
@@ -293,8 +296,102 @@ int main(int argc, char **argv){
 
 	for (int i=0;i<255;i++)
 	{
-		printf("location %d, %f \n" ,i,CH1.obj[i].power);
+		printf("location %d, %f,%f,%f \n" ,i,CH1.obj[i].ref,CH1.obj[i].etalon,CH1.obj[i].power);
 	}
+	int differ=-128+data;
+	if (differ<0)
+	{
+		for (i=0;i<abs(differ)*3/4;i++)
+		{
+			seq_gen_mc(Negshi20,810,1);
+			usleep(68800);
+		}
+		
+	}
+	else
+	{
+		for (i=0;i<abs(differ)*3/4;i++)
+		{
+			seq_gen_mc(PosShi20,810,1);
+			usleep(68800);
+		}
+	}
+	
+	
+	
+	
+	
+	for (c=0;c<5;c++)
+	{
+		avr=0;
+		if(c==1 || c==2)
+		{
+			seq_gen_mc(PosShi20,810,1);
+			usleep(68800);
+		}
+		else if(c==3)
+		{
+			seq_gen_mc(Negshi20,810,1);
+			usleep(68800);
+			seq_gen_mc(Negshi20,810,1);
+			usleep(68800);
+			seq_gen_mc(Negshi20,810,1);
+			usleep(68800);
+			
+		}
+		else if(c==4)
+		{
+			seq_gen_mc(Negshi20,810,1);
+			usleep(68800);
+		}
+		
+			 
+        for (i=0 ;i<50;i++)
+        {
+         	 rp_AcqReset();
+
+             rp_AcqSetDecimation(RP_DEC_1024);
+
+             rp_AcqSetSamplingRate(RP_SMP_122_070K);
+
+
+             rp_AcqStart();
+
+
+             usleep(68800);
+
+             rp_AcqStop();
+
+
+             rp_AcqGetOldestDataV(RP_CH_1, &Num_SAMPLES, buff);
+
+
+			 rp_AcqGetOldestDataV(RP_CH_2, &Num_SAMPLES, buff2);
+
+   			 filler(buf,buff,Num_SAMPLES);
+
+             fft(buf,Num_SAMPLES);
+
+			 filler(buf2,buff2,Num_SAMPLES);
+			 
+			 fft(buf2,Num_SAMPLES);
+
+             float REF=maxia(cabsf(buf[j]),cabsf(buf[j-1]),cabsf(buf[j+1]));
+
+
+             float ETA=maxia(cabsf(buf2[j]),cabsf(buf2[j-1]),cabsf(buf2[j+1]));
+
+             avr=avr+ETA/REF;
+        }
+			Demoarray[c]=avr/50;
+	
+	}// end of the for loop
+
+	seq_gen_mc(PosShi20,810,1);
+	usleep(68800);
+	seq_gen_mc(PosShi20,810,1);
+	usleep(68800);
+		
 	CH1.CurStat='4';
 
 	break ;
@@ -342,7 +439,28 @@ int main(int argc, char **argv){
 		
 		avr=avr+ETA/REF;
 	}
-		printf("%f \n",avr/50);
+	
+	for (c=0;c<5;c++)
+		printf ("%d   %f   \n",c,Demoarray[c]);
+
+		if((avr/50)>Demoarray[4])
+		{
+			printf("shift to positive\n");
+			seq_gen_mc(PosShi20,810,1);
+		}
+
+		else if((avr/50)<Demoarray[2])
+		{
+			printf("shift to negative \n");
+			seq_gen_mc(Negshi20,810,1);
+		}
+		else 
+		{
+			printf("still in locked stage \n");
+		}
+		printf("Demoarray %f \n",Demoarray[0]);
+		printf("Current value %f \n",avr/50);
+
 	 break ;
 			
 			
